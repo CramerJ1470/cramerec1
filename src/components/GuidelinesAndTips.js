7d928b5c3013ef436267f0d356d3a0fd768d0cb5
cramerj1470 <john@Cramerec.com>
2023-02-12 12:13:18 -0500

    finished guidelines and tips page

--- src/components/GuidelinesAndTips.js
+++ src/components/GuidelinesAndTips.js
@@ -10,157 +10,322 @@ const GuidelinesAndTips = () => {
 			<Logo />
 			<Banner />
 			<div className="blackbackground">
-			<div className="scrolltip">
-				<h2>
-					TIPS FOR HOMEOWNERS THAT CAN LESSEN THE STRESS<br></br> OF A
-					REMODELING PROJECT
-				</h2>
+				<div className="scrolltip">
+					<h2>
+						TIPS FOR HOMEOWNERS THAT CAN LESSEN THE STRESS<br></br>{" "}
+						OF A REMODELING PROJECT
+					</h2>
 
-				<div className="guideline">
-					<p className="pl20">
-						NYC/NARI encourages homeowners to work with their
-						contractor to ensure smooth sailing during remodeling
-						work. The homeowneer's role in the remodeling process
-						doesn't end once a reputable professional has been
-						hired. Simple steps initiated by the homeowner will
-						lessen the stress of a remodeling project over the next
-						few days, weeks or months.
-					</p>
-					<p className="pl20">
-						NARI suggest the following tips to create a positive
-						experience for the homeowner and the contractor
-					</p>
-					<span className="info">
-						<div className="infocircle">
-							<h1>1</h1>
-						</div>
-						<div className="rel">
-							<h3 className="about">Plan Ahead</h3>
-							<ul>
-								<li>
-									<i
-										class="fa fa-caret-right"
-										aria-hidden="true"
-									></i><strong>
-									  No Water or Power
-									  </strong>
-								</li>
-							</ul>
-							<p className="movedover">
-								Know in advance if and when the workers plan to
-								shut off your powwr and water.You can avoid food
-								spoilage in the refrigerator/freezer. Also, be
-								prepared by having a supply of bottled water on
-								hand.
-							</p>
-							<ul>
-								<li>
-									<i
-										class="fa fa-caret-right"
-										aria-hidden="true"
-									></i><strong>
-									 Create a Phased List of Procedures
-									 </strong>
-								</li>
-							</ul>
-							<p className="movedover">
-								Meet with the contractor and create a list of work to be done. Try to incorporate a chronological timeline so you'll know what phase of the project you can expext next.
-							</p>
-							<ul>
-								<li>
-									<i
-										class="fa fa-caret-right"
-										aria-hidden="true"
-									></i><strong>
-									 Make an Extra Key or Create a Temporary Security Code
-									 </strong>
-								</li>
-							</ul>
-							<p className="movedover">
-								Depending on your schedule, there may be a need to share keys or opener/security codes with the contractor or workers. Keep track of any keys given out. If you give an opener or security door code, create a temporary code while the work is in progress. Once the work is complete, return to your regular code. Don't forget to ask who will take responsibility for any mishaps.
-							</p>
-						</div>
-					</span>
-					<span className="info">
-						<div className="infocircle">
-							<h1>2</h1>
-						</div>
-						<div className="rel">
-						<h3 className="about">Double Check All Materials Upon Arrival</h3>
-							<ul>
-								<li>
-									<i
-										class="fa fa-caret-right"
-										aria-hidden="true"
-									></i>{" "}
-									Is That The Right Color?
-								</li>
-							</ul>
-							<p className="movedover">
-								Once the cabinets, baseboards are installed, there's no turning back (or at least it can be very expensive to have to do so). Before workers start drilling, gluing, nailing, painting, etc. make sure the products you ordered matches the product that was delivered to your home- and don't just trust the box it came in. Open it! 
-							</p>
-							<ul>
-								<li>
-									<i
-										class="fa fa-caret-right"
-										aria-hidden="true"
-									></i>
-									'Don't Plan to Buy as You Go'
-								</li>
-							</ul>
-							<p className="movedover">
-								You can cut down last minute stressful decisions as well as material availability delays by selecting all materials with your contractor in advance.
-							</p>
-						</div>
-					</span>
-					<span className="info">
-						<div className="infocircle">
-							<h1>3</h1>
-						</div>
-						<div className="rel">
-							<h3 className="about">Of Course</h3>
-							<ul>
-								<li>
-									<i
-										class="fa fa-caret-right"
-										aria-hidden="true"
-									></i>{" "}
-									No Water or Power
-								</li>
-							</ul>
-							<p className="movedover">
-								We strive to exceed our clients' expectations.
-								Each member of our team is chosen for their
-								ability to serve every client and their home
-								with the utmost respect and courtesy.
-							</p>
-						</div>
-					</span>
-					<span className="info">
-						<div className="infocircle">
-							<h1>4</h1>
-						</div>
-						<div className="rel">
-							<h3 className="about">Not Done Yet</h3>
-							<ul>
-								<li>
-									<i
-										class="fa fa-caret-right"
-										aria-hidden="true"
-									></i>{" "}
-									No Water or Power
-								</li>
-							</ul>
-							<p className="movedover">
-								We strive to exceed our clients' expectations.
-								Each member of our team is chosen for their
-								ability to serve every client and their home
-								with the utmost respect and courtesy.
-							</p>
-						</div>
-					</span>
+					<div className="guideline">
+						<p className="pl20">
+							NYC/NARI encourages homeowners to work with their
+							contractor to ensure smooth sailing during
+							remodeling work. The homeowneer's role in the
+							remodeling process doesn't end once a reputable
+							professional has been hired. Simple steps initiated
+							by the homeowner will lessen the stress of a
+							remodeling project over the next few days, weeks or
+							months.
+						</p>
+						<p className="pl20">
+							NARI suggest the following tips to create a positive
+							experience for the homeowner and the contractor
+						</p>
+						<span className="info">
+							<div className="infocircle">
+								<h1>1</h1>
+							</div>
+							<div className="rel">
+								<h3 className="about">PLAN AHEAD</h3>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										<strong>No Water or Power</strong>
+									</li>
+								</ul>
+								<p className="movedover">
+									Know in advance if and when the workers plan
+									to shut off your powwr and water.You can
+									avoid food spoilage in the
+									refrigerator/freezer. Also, be prepared by
+									having a supply of bottled water on hand.
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										<strong>
+											Create a Phased List of Procedures
+										</strong>
+									</li>
+								</ul>
+								<p className="movedover">
+									Meet with the contractor and create a list
+									of work to be done. Try to incorporate a
+									chronological timeline so you'll know what
+									phase of the project you can expext next.
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										<strong>
+											Make an Extra Key or Create a
+											Temporary Security Code
+										</strong>
+									</li>
+								</ul>
+								<p className="movedover">
+									Depending on your schedule, there may be a
+									need to share keys or opener/security codes
+									with the contractor or workers. Keep track
+									of any keys given out. If you give an opener
+									or security door code, create a temporary
+									code while the work is in progress. Once the
+									work is complete, return to your regular
+									code. Don't forget to ask who will take
+									responsibility for any mishaps.
+								</p>
+							</div>
+						</span>
+						<span className="info">
+							<div className="infocircle">
+								<h1>2</h1>
+							</div>
+							<div className="rel">
+								<h3 className="about">
+									DOUBLE CHECK ALL MATERIALS UPON ARRIVAL
+								</h3>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Is That The Right Color?
+									</li>
+								</ul>
+								<p className="movedover">
+									Once the cabinets, baseboards are installed,
+									there's no turning back (or at least it can
+									be very expensive to have to do so). Before
+									workers start drilling, gluing, nailing,
+									painting, etc. make sure the products you
+									ordered matches the product that was
+									delivered to your home- and don't just trust
+									the box it came in. Open it!
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										'Don't Plan to Buy as You Go'
+									</li>
+								</ul>
+								<p className="movedover">
+									You can cut down last minute stressful
+									decisions as well as material availability
+									delays by selecting all materials with your
+									contractor in advance.
+								</p>
+							</div>
+						</span>
+						<span className="info">
+							<div className="infocircle">
+								<h1>3</h1>
+							</div>
+							<div className="rel">
+								<h3 className="about">
+									BATTEN DOWN YOUR BELONGINGS
+								</h3>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Cover Furniture and Carpeting
+									</li>
+								</ul>
+								<p className="movedover">
+									Your contractor will take the necessary
+									steps to portect and cover the work area,
+									but remodeling leaves more dirt and dust
+									throughtout the house than you can imagine.
+									You may want to cover funiture in other
+									areas of the house, especially in rooms
+									adjacent to work being performed.
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Dry Clean Drapes, Rugs and Upholstery
+									</li>
+								</ul>
+								<p className="movedover">
+									US Use remodeling time as a perfect excuse
+									to have curtains, rugs and upholstery
+									cleaned. Otherwise remove drapes, throw rugs
+									and upholsteryfurniture as a practical and
+									place them in an area away from the work
+									being performed.
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Store Breakables
+									</li>
+								</ul>
+								<p className="movedover">
+									Accidents happen. Bow away knick knacks and
+									personal mementos from the construction zone
+									for safe keeping. If furniture is to be
+									moved, pack contents in boxes and remove
+									them from the work area. This protects
+									contents from loss and breakage, and also
+									prevents the homeowner having to dust or
+									clean each knick knacj after the work is
+									complete.
+								</p>
+							</div>
+						</span>
+						<span className="info">
+							<div className="infocircle">
+								<h1>4</h1>
+							</div>
+							<div className="rel">
+								<h3 className="about">
+									STAY OUT OF CONSTRUCTION AREA
+								</h3>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Safety First
+									</li>
+								</ul>
+								<p className="movedover">
+									Keep children, toys and pets away from the
+									work area
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Maintain Schedule
+									</li>
+								</ul>
+								<p className="movedover">
+									Enter the work area when workers are on a
+									break or after they have finished for the
+									day to eliminate disruptions and injury to
+									the homeowner or workers. This is important
+									for maintaining construction schedules as
+									well as safety for the homeowner and
+									workers.
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Create Space
+									</li>
+								</ul>
+								<p className="movedover">
+									Make Adequate room for tools, equipment,
+									materials and workers. Remove your items
+									from the work area so the contractor has
+									sufficient space to work.
+								</p>
+							</div>
+						</span>
+						<span className="info">
+							<div className="infocircle">
+								<h1>5</h1>
+							</div>
+							<div className="rel">
+								<h3 className="about">BE HOSPITABLE</h3>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Basic Human Relations
+									</li>
+								</ul>
+								<p className="movedover">
+									Realize that workers ar going to be a part
+									of your lives for a period of time. Win
+									cooperation from workers and the contractor
+									with mutual respect and honesty.
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Refreshments
+									</li>
+								</ul>
+								<p className="movedover">
+									It's a nice gesture for hard work. An
+									unexpected pot of coffee or a cold glass of
+									lemonade will always get a "thank you".
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Give Compliments
+									</li>
+								</ul>
+								<p className="movedover">
+									Nothing makes anyone work harder than a
+									sincere nice word od appreciation and
+									gratitude.
+								</p>
+								<ul>
+									<li>
+										<i
+											class="fa fa-caret-right"
+											aria-hidden="true"
+										></i>
+										Be Cool and Calm
+									</li>
+								</ul>
+								<p className="movedover">
+									Take a deep breath, step back and trust his
+									or her skills
+								</p>
+							</div>
+						</span>
+					</div>
 				</div>
-			</div>
 			</div>
 		</>
 	);
