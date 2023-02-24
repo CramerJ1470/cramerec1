223b684dfda011e957a4056704c3faef67c8eab1
cramerj1470 <john@Cramerec.com>
2023-02-24 13:37:00 -0500

    contact US page

--- src/App.js
+++ src/App.js
@@ -52,6 +52,17 @@ function App() {
 	const [uniquefeatures, setUniquefeatures] = useState([]);
 
 	useEffect(() => {
+		if (document) {
+		  const stylesheet = document.createElement("link");
+		  stylesheet.rel = "stylesheet";
+		  stylesheet.href = "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+	
+		  document.head.appendChild(stylesheet);
+		}
+	  }, []);
+
+
+	useEffect(() => {
 		getBeforeafters(setBeforeafters);
 	}, []);
 
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,77 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import Banner from "./Banner";
 
-class ContactUs extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			name: "",
-			email: "",
-			message: "",
-		};
-	}
-	render() {
-		return (
-			<>
-			<span className="disp">
-			<div className="worklinks">Contact Us Under Construction</div>
-			<Logo />
-			<Banner />
-		</span>
-			
-				{/* <form
-					id="contact-form"
-					onSubmit={this.handleSubmit.bind(this)}
-					method="POST"
-				>
-					<div className="form-group">
-						<label htmlFor="name">Name</label>
-						<input
-							type="text"
-							className="form-control"
-							value={this.state.name}
-							onChange={this.onNameChange.bind(this)}
-						/>
-					</div>
-					<div className="form-group">
-						<label htmlFor="exampleInputEmail1">
-							Email address
-						</label>
-						<input
-							type="email"
-							className="form-control"
-							aria-describedby="emailHelp"
-							value={this.state.email}
-							onChange={this.onEmailChange.bind(this)}
-						/>
-					</div>
-					<div className="form-group">
-						<label htmlFor="message">Message</label>
-						<textarea
-							className="form-control"
-							rows="5"
-							value={this.state.message}
-							onChange={this.onMessageChange.bind(this)}
-						/>
-					</div>
-					<button type="submit" className="btn btn-primary">
-						Submit
-					</button>
-				</form> */}
-				</>
-			
-		);
-	}
-	onNameChange(event) {
-		this.setState({ name: event.target.value });
-	}
-	onEmailChange(event) {
-		this.setState({ email: event.target.value });
-	}
-	onMessageChange(event) {
-		this.setState({ message: event.target.value });
-	}
-	handleSubmit(event) {}
+const FORM_ENDPOINT = ""; 
+
+
+const ContactUs = () => {
+const [submitted, setSubmitted] = useState(false);
+const handleSubmit = () => {
+  setTimeout(() => {
+	setSubmitted(true);
+  }, 100);
+};
+
+if (submitted) {
+  return (
+	<div>
+		<div className="py-6">
+	  <div className="text-2xl">Thank you!</div>
+	  <div className="text-md">We'll be in touch soon.</div>
+	</div>
+	</div>
+  );
 }
+
+return (
+	<>
+	
+	<span className="disp">
+	
+	<Logo />
+	<Banner />
+</span>
+<div className="py-6">
+	<div className="contactForm">
+  <form
+	action={FORM_ENDPOINT}
+	onSubmit={handleSubmit}
+	method="POST"
+	target="_blank"
+  >
+	<div className="mb-3 pt-0">
+	  <input
+		type="text"
+		placeholder="Your name"
+		name="name"
+		className="px-3 py-3 placeholder-white text-slate-600 relative bg-white bg-gray-400 rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+		required
+	  />
+	</div>
+	<div className="mb-3 pt-5">
+	  <input
+		type="email"
+		placeholder="Email"
+		name="email"
+		className="px-3 py-3 placeholder-white text-slate-600 relative bg-white bg-gray-400 rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+		required
+	  />
+	</div>
+	<div className="mb-3 pt-5">
+	  <textarea
+		placeholder="Your message"
+		name="message"
+		className="px-3 py-3 placeholder-white text-slate-600 relative bg-white bg-gray-400 rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+		required
+	  />
+	</div>
+	<div className="mb-3 pt-5 ">
+	  <button
+		className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+		type="submit"
+	  >
+		Send a message
+	  </button>
+	</div>
+  </form>
+  </div>
+  </div>
+  </>
+);
+};
+		
 export default ContactUs;
